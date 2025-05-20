b2d89f42d0404cfd95f1c7f3e8faa16e1f713198
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-05-20 22:26:35 +0530

    axios and fetch

--- src/screens/AxiosScreen.tsx
+++ src/screens/AxiosScreen.tsx
@@ -1,6 +1,53 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+interface Post {
+  id: number;
+  title: string;
+}
+
+const api = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
+
+// request
+api.interceptors.request.use(config => {
+  console.log('Request Sent : ', config);
+  return config;
+});
+
+// response
+api.interceptors.response.use(response => {
+  console.log('Request Received : ', response);
+  return response;
+});
+
 const AxiosScreen: React.FC = () => {
+  const [data, setData] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchListPost = async () => {
+    try {
+      setLoading(true);
+      const response = await api.get<Post[]>('/posts');
+      if (response) {
+        setData(response.data);
+        setLoading(false);
+      } else {
+        setData([]);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(data, 'axios');
+
+  useEffect(() => {
+    fetchListPost();
+  }, []);
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Axios Screen</Text>
--- src/screens/DataFetching.tsx
+++ src/screens/DataFetching.tsx
@@ -1,12 +1,65 @@
-import {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+interface Post {
+  id: number;
+  title: string;
+}
 
 const DataFetching: React.FC = () => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchListPost = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+      );
+      const data: Post[] = await response.json();
+      if (data) {
+        setData(data);
+        setLoading(false);
+      } else {
+        setData([]);
+        setLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchListPost();
+  }, []);
+
+  console.log(data);
+  const renderItem = ({item}: {item: Post}) => {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{item.title}</Text>
+      </View>
+    );
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Data Fetching using Fetch API</Text>
+      {loading ? (
+        <ActivityIndicator size={'large'} color={'#0000ff'} />
+      ) : (
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={item => item.id.toString()}
+        />
+      )}
     </View>
   );
 };
@@ -18,6 +71,16 @@ const styles = StyleSheet.create({
   },
   header: {
     fontSize: 20,
+    fontWeight: 'bold',
+  },
+  item: {
+    backgroundColor: '#c6df0a',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 16,
     fontWeight: 'bold',
   },
 });
