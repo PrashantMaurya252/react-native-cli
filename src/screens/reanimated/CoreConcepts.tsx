7b6e52d24ab9ebf177aec58216f58b452fd6eb53
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-05-24 22:36:41 +0530

    build a screen using reanimation

--- src/screens/reanimated/CoreConcepts.tsx
+++ src/screens/reanimated/CoreConcepts.tsx
@@ -1,7 +1,14 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import Animated, {
+  cancelAnimation,
+  Easing,
   useAnimatedRef,
+  useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
+  withRepeat,
+  withSpring,
+  withTiming,
 } from 'react-native-reanimated';
 
 const CoreConcept: React.FC = () => {
@@ -9,16 +16,68 @@ const CoreConcept: React.FC = () => {
   const rotation = useSharedValue(0);
   const scale = useSharedValue(1);
 
+  const opacity = useDerivedValue(() => {
+    return Math.sin((rotation.value * Math.PI) / 180) / 2 + 0.5;
+  });
+
   const animatedRef = useAnimatedRef<Animated.View>();
+
+  const boxStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: offset.value,
+        },
+        {
+          rotate: `${rotation.value}deg`,
+        },
+        {
+          scale: scale.value,
+        },
+      ],
+      opacity: opacity.value,
+    };
+  });
+
+  const textStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: 1 / scale.value,
+        },
+      ],
+    };
+  });
+  const handleStartAnimation = () => {
+    offset.value = withSpring(Math.random() * 200 - 100);
+    rotation.value = withRepeat(
+      withTiming(360, {duration: 2000, easing: Easing.linear}),
+      -1,
+      false,
+    );
+
+    scale.value = withRepeat(withTiming(1.5, {duration: 1000}), -1, true);
+  };
+
+  const handleStopAnimation = () => {
+    cancelAnimation(offset);
+    cancelAnimation(rotation);
+    cancelAnimation(scale);
+  };
   return (
     <View style={styles.container}>
       <Text style={styles.headerText}>React Animated Core Concepts</Text>
-      <Animated.View style={[styles.box]} ref={animatedRef}>
-        <Animated.Text style={styles.boxText}>Animated Box</Animated.Text>
+      <Animated.View style={[styles.box, boxStyle]} ref={animatedRef}>
+        <Animated.Text style={[styles.boxText, textStyle]}>
+          Animated Box
+        </Animated.Text>
       </Animated.View>
       <View style={styles.btnContainer}>
-        <Pressable style={styles.btn}>
+        <Pressable onPress={handleStartAnimation} style={styles.btn}>
           <Text style={styles.btnText}>Start Animation</Text>
+        </Pressable>
+        <Pressable onPress={handleStopAnimation} style={styles.btn}>
+          <Text style={styles.btnText}>Stop Animation</Text>
         </Pressable>
       </View>
     </View>
